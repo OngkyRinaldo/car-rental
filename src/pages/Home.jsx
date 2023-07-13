fb11b35502dddb6bbd0f3c82c03dd57d9b7d71d2
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-13 22:10:39 +0700

    update book component at home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Book from '../components/home/book/Book';
 import Hero from '../components/home/hero/Hero';
 
 const Home = () => {
     return (
         <main>
             <Hero />
+            <Book />
         </main>
     );
 };
