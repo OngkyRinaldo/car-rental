90aa5fbc21fdb5ac8c8a4062b36a8559db8243cb
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-13 22:27:21 +0700

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './components/navbar/Navbar';
 import { useState } from 'react';
+import Navbar from './components/navbar/Navbar';
 import Menu from './components/navbar/Menu';
-
-import Footer from './components/footer/Footer';
+import Home from './pages/Home';
 import About from './pages/About';
 import Vechicle from './pages/Vechicle';
 import Teams from './pages/Teams';
+import Contact from './pages/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
     const [clicked, isClicked] = useState(false);
@@ -20,6 +20,7 @@ const App = () => {
                 <Route path='/about' element={<About />} />
                 <Route path='/vechicle' element={<Vechicle />} />
                 <Route path='/teams' element={<Teams />} />
+                <Route path='/contact' element={<Contact />} />
             </Routes>
 
             <Footer />
