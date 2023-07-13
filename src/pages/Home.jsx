81c9aa2aa560bbb1a28101ce0a850b5d8d1abccf
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-13 22:00:25 +0700

    update Book

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import Banner from '../components/home/banner/Banner';
-import Book from '../components/home/book/book';
+import Book from '../components/home/book/Book';
 import CarsList from '../components/home/carList/CarsList';
 import Faq from '../components/home/faq/Faq';
 import Hero from '../components/home/hero/Hero';
