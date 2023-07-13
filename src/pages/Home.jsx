d938ee18c0ee0cd8a72bdecc2054c509ea5963d1
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-13 22:02:44 +0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,9 @@
-import Banner from '../components/home/banner/Banner';
-import Book from '../components/home/book/Book';
-import CarsList from '../components/home/carList/CarsList';
-import Faq from '../components/home/faq/Faq';
 import Hero from '../components/home/hero/Hero';
 
 const Home = () => {
     return (
         <main>
             <Hero />
-            <Book />
-            <Banner />
-            <CarsList />
-            <Faq />
         </main>
     );
 };
